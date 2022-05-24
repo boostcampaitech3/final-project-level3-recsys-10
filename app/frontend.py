35b4ba1a2242fa43164db3025a49fc40e2d84213
minchoul2 <sny2128@gmail.com>
2022-05-24 11:35:45 +0000

    [feat/#5] Fix coldstart section

--- app/frontend.py
+++ app/frontend.py
@@ -1,4 +1,12 @@
+import sys
+
 import streamlit as st
+import urllib.request
+from io import BytesIO
+from PIL import Image
+
+#################################
+# 맥주 DB
 
 # 맥주 id-name dict
 beer_dict = {'729':'Sapporo Premium Beer / Draft Beer', '567803':'Tsingtao Premium Stout 4.8%',
@@ -6,34 +14,49 @@ beer_dict = {'729':'Sapporo Premium Beer / Draft Beer', '567803':'Tsingtao Premi
 '34662':'Heineken Dark Lager', '48076':'Heineken Premium Light'}
 
 beer_list = ['Sapporo Premium Beer / Draft Beer', 'Tsingtao Premium Stout 4.8%', 'Tsingtao Draft Beer 11º (Pure Draft Beer)',
-'Heineken', 'Heineken Dark Lager', 'Heineken Premium Light']
+'Heineken', 'Heineken Dark Lager', 'Heineken Premium Light', 'Heineken Premium Light', 'Heineken Premium Light']
 
 beer_img_link = ['https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_729',
 'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_567803',
 'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_64518',
 'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_37',
 'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_34662',
+'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
+'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
 'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076']
+
+##################################
+
+# 변수
+options = ["좋아요😊", "몰라요🙄", "싫어요😠"]
+image_size = [150,420]
+
+def resize_image(image_path,image_size):
+    # 동일한 이미지로 만들기 위한 함수
+    req = urllib.request.Request(image_path)
+    res = urllib.request.urlopen(req).read()
+    image = Image.open(BytesIO(res))
+    resized_image = image.resize(image_size)
+    return resized_image     
+
 
 st.title("도전맥주홀릭")
 
-# 이전 버전에서 사용 가능
-# grid = st.grid()
+st.header("맥주에 대해 평가해 주세요")
 
-# for image_info in beer_img_link:
-#   row = grid.row()
-#   row.image(image_info.image)
-
-options = ["좋아요😊", "몰라요🙄", "싫어요😠"]
-
+# streamlit은 grid 방식을 지원하지 않기 때문에 columns로 hard coding
 col1, col2, col3, col4 = st.columns(4)
-col1.image(beer_img_link[0])
-col1.write(st.radio(
-    "해당 맥주에 대해 어떻게 생각하시나요?!", options
-))
-col2.write("this is col2")
-col3.write("this is col3!!!")
-col4.write("this is col4~~~")
+col5, col6, col7, col8 = st.columns(4)
+cols = [col1, col2, col3, col4, col5, col6, col7, col8]
+
+
+for col, beer, image_path in zip(cols,beer_list,beer_img_link):
+    with col:
+        st.image(resize_image(image_path, image_size))
+        st.write(st.radio(
+            beer, options, key = col
+        ))
+
 
 # submit 버튼 어떤 것으로 해야할지 search 필요
 st.button('제출')
