53b3861a1c2e07f51161b571aa47fcbde0d54216
dongseok1541 <lds2201@naver.com>
2022-05-26 11:12:25 +0000

    Revert "Revert "[feat/$11] MVP Done!""
    This reverts commit d4221891187f5a4f0a0890bf622ffe53b0ce98bd.

--- frontend/config.py
+++ frontend/config.py
@@ -2,7 +2,7 @@ class CGF:
 
     # coldstart 칼럼과 아이템 개수 
     max_col = 4
-    num_items = 12 # max_col의 배수로 지정해주세요
+    num_items = 8 # max_col의 배수로 지정해주세요
 
     # radio-box 에 들어갈 옵션
     options = ["좋아요😊", "몰라요🙄", "싫어요😠"]
@@ -11,9 +11,9 @@ class CGF:
     image_size = [150,420]
 
     # 좋아요 싫어요 몰라요 변형점수
-    LIKE = 5
-    UNLIKE = 1
-    UNKNOWN = -1
+    LIKE = 5.0
+    UNLIKE = 1.0
+    UNKNOWN = -1.0
 
     # 추천 키워드
     rec_keywords = ["새로운",
--- frontend/ui.py
+++ frontend/ui.py
@@ -4,34 +4,19 @@ import streamlit as st
 from requests import request
 import requests
 
-from utils import post_feedback, resize_image, save_rate,get_recommended_beer,get_info,get_grid,get_cols,post_feedback, get_init_beers
+from utils import get_beer_info, post_feedback, resize_image, save_rate,get_recommended_beer,get_info,get_grid,get_cols,post_feedback, get_init_beers
 from config import CGF
 
 #################################
 # 맥주 DB
 # TODO DB를 연동해 주세요
 
-beer_dict = {'Sapporo Premium Beer / Draft Beer':'729', 'Tsingtao Premium Stout 4.8%':'567803',
-'Tsingtao Draft Beer 11º (Pure Draft Beer)':'64518', 'Heineken':'37',
-'Heineken Dark Lager':'34662', 'Heineken Premium Light':'48076'}
+# beer_dict = {'Sapporo Premium Beer / Draft Beer':'729', 'BrewDog Punk IPA':'135361',
+# 'Burgemeester':'36249', 'Heineken':'37',
+# 'Heineken Dark Lager':'34662', 'Apple Fox':'614833'}
 
-beer_list = ['Sapporo Premium Beer / Draft Beer', 'Tsingtao Premium Stout 4.8%', 'Tsingtao Draft Beer 11º (Pure Draft Beer)',
-'Heineken', 'Heineken Dark Lager', 'Heineken Premium Light', 'Heineken Premium Light', 'Heineken Premium Light', 'Heineken Premium Light', 
-'Heineken Premium Light', 'Heineken Premium Light', 'Heineken Premium Light']
-
-beer_img_link = ['https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_729',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_567803',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_64518',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_37',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_34662',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076',
-'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_48076']
+# beer_list = ['Sapporo Premium Beer / Draft Beer', 'Burgemeester','BrewDog Punk IPA',
+# 'Heineken', 'Apple Fox']
 
 #################################
 # 저장변수
@@ -46,18 +31,24 @@ st.title("도전맥주홀릭")
 st.header("맥주에 대해 평가해 주세요")
 
 # streamlit은 grid 방식을 지원하지 않기 때문에 columns로 hard coding 
+beer_df = get_beer_info()
 row, col = get_grid(CGF.num_items)
 total_cols = get_cols(row, col)
-beer_list ,beer_img_link =get_init_beers(beer_list, beer_dict)
-
+# beer_list ,beer_img_link =get_init_beers(beer_list, beer_dict)
+beer_list ,beer_img_link =get_init_beers(beer_df)
+# print("---beer_list------")
+# print(beer_list)
+# print("----ber_img_link------")
+# print(beer_img_link)
 for col, beer, image_path in zip(total_cols, beer_list , beer_img_link): 
     # TODO beer_list,beer_img_link 는 DP와 연동해주세요
+    # beer : BeerName , str
     with col:
         st.image(resize_image(image_path, CGF.image_size))
         option_rate = st.radio(
             beer, CGF.options, key = col
         )
-        save_rate(beer_dict, beer, option_rate, coldstart_data)
+        save_rate(beer_df, beer, option_rate, coldstart_data)
 
 # 추천 키워드 선택
 st.header("추천에 원하는 키워드를 선택해보세요")
@@ -67,22 +58,24 @@ keywords = st.multiselect('여러개 선택가능!', CGF.rec_keywords)
 coldstart_button = st.button('제출')
 
 if coldstart_button: 
-    st.write(coldstart_data)
-    st.write(keywords)
+    # st.write(coldstart_data)
+    # st.write(keywords)
     # TODO 여기서 POST를 해주세요
-    
-
+    data_load_state = st.text('맥주 추천중...')
+    response = requests.post("http://localhost:8001/select", json=coldstart_data)
+    response = response.json()
     # 4캔 추천 반환
+    data_load_state.text('')
     st.header("추천결과!")
-    r_beer_id = get_recommended_beer()
-    r_beers = {beer_id:get_info(beer_id) for beer_id in r_beer_id}
+    r_beer_id = get_recommended_beer(response)
+    r_beers = {beer_id:get_info(beer_id, beer_df) for beer_id in r_beer_id}
 
     r_col1, r_col2, r_col3, r_col4 = st.columns(4)
     r_cols = [r_col1, r_col2, r_col3, r_col4]
     for col, beer_id in zip(r_cols, r_beer_id):
         with col:
-            st.image(resize_image(r_beers[beer_id]['imageUrl'], CGF.image_size),
-                    caption = r_beers[beer_id]['beerName'])
+            st.image(resize_image(r_beers[beer_id]['imageUrl'], CGF.image_size))
+            st.write(r_beers[beer_id]['beerName'])
 
 
     # 추천 결과 평가(TODO 수정해야함 - if문 안에서 버튼은 다음 액션으로 이어지지 않음)
