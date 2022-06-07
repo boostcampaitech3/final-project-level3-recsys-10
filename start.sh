a42e894c7b24d710804f03a0151e4e955687b1ef
dongseoklee <41297473+dongseoklee1541@users.noreply.github.com>
2022-06-07 10:49:46 +0900

    [feat/#37] Complete Docker

--- start.sh
+++
@@ -1,4 +0,0 @@
-apt-get update -y 
-apt-get install wget unzip -y
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1UVikLdlIfpISqxxqFXeBX_5B_KkHec11' -O required_files.zip
-unzip required_files.zip
